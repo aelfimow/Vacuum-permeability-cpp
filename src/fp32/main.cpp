2169447b650783bbfb5604fcf4c586d09991e589
Alexander Elfimow <my.free.mail@gmx.de>
2024-09-09 19:03:24 +0200

    Use new functions

--- src/fp32/main.cpp
+++ src/fp32/main.cpp
@@ -6,11 +6,14 @@
 using fp32 = float;
 static_assert(sizeof(fp32) == 4U);
 
+extern fp32 speed_of_light();
+extern fp32 vacuum_permittivity();
+
 int main(int, char *[])
 {
-    fp32 const c = 299792458.0f;
+    fp32 const c = speed_of_light();
 
-    fp32 const eps0 = 8.8541878128E-12f;
+    fp32 const eps0 = vacuum_permittivity();
 
     fp32 const mu0 = 1.0f / (eps0 * (c * c));
 
