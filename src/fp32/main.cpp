75d8cb36abefd032d34583ae6e362024234cdf27
Alexander Elfimow <my.free.mail@gmx.de>
2024-09-10 20:13:04 +0200

    fp32: compute values

--- src/fp32/main.cpp
+++ src/fp32/main.cpp
@@ -12,12 +12,40 @@ extern fp32 vacuum_permeability();
 int main(int, char *[])
 {
     fp32 const c = speed_of_light();
-
     fp32 const eps0 = vacuum_permittivity();
+    fp32 const mu0 = vacuum_permeability();
 
-    fp32 const mu0 = 1.0f / (eps0 * (c * c));
+    // Print out hard-coded values:
+    {
+        std::cout << "Hard coded values (fp32):" << std::endl;
 
-    std::cout << std::setprecision(10) << "Vacuum magnetic permeability (fp32): " << mu0 << std::endl;
+        std::cout << std::setprecision(10) << "    Speed of light: " << c << std::endl;
+        std::cout << std::setprecision(10) << "    Vacuum permittivity: " << eps0 << std::endl;
+        std::cout << std::setprecision(10) << "    Vacuum permeability: " << mu0 << std::endl;
+    }
+
+    std::cout << "Computed values (fp32):" << std::endl;
+
+    // Compute speed of light:
+    {
+        fp32 const computed_c = 1.0f / ::sqrtf(eps0 * mu0);
+
+        std::cout << std::setprecision(10) << "    Speed of light: " << computed_c << std::endl;
+    }
+
+    // Compute vacuum permittivity:
+    {
+        fp32 const computed_eps0 = 1.0f / (mu0 * (c * c));
+
+        std::cout << std::setprecision(10) << "    Vacuum permittivity: " << computed_eps0 << std::endl;
+    }
+
+    // Compute vacuum permeability:
+    {
+        fp32 const computed_mu0 = 1.0f / (eps0 * (c * c));
+
+        std::cout << std::setprecision(10) << "    Vacuum permeability: " << computed_mu0 << std::endl;
+    }
 
     return EXIT_SUCCESS;
 }
