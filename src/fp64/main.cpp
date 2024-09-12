2f5bd01a3b48ffc95d625a2fcc9a01521689f1db
Alexander Elfimow <my.free.mail@gmx.de>
2024-09-12 20:32:47 +0200

    fp64: added digits

--- src/fp64/main.cpp
+++ src/fp64/main.cpp
@@ -11,6 +11,7 @@ extern fp64 vacuum_permeability();
 
 int main(int, char *[])
 {
+    int const digits = 16;
     fp64 const c = speed_of_light();
     fp64 const eps0 = vacuum_permittivity();
     fp64 const mu0 = vacuum_permeability();
@@ -19,9 +20,9 @@ int main(int, char *[])
     {
         std::cout << "Hard coded values (fp64):" << std::endl;
 
-        std::cout << std::setprecision(10) << "    Speed of light: " << c << std::endl;
-        std::cout << std::setprecision(10) << "    Vacuum permittivity: " << eps0 << std::endl;
-        std::cout << std::setprecision(10) << "    Vacuum permeability: " << mu0 << std::endl;
+        std::cout << std::setprecision(digits) << "    Speed of light: " << c << std::endl;
+        std::cout << std::setprecision(digits) << "    Vacuum permittivity: " << eps0 << std::endl;
+        std::cout << std::setprecision(digits) << "    Vacuum permeability: " << mu0 << std::endl;
     }
 
     std::cout << "Computed values (fp64):" << std::endl;
@@ -30,21 +31,21 @@ int main(int, char *[])
     {
         fp64 const computed_c = 1.0 / ::sqrt(eps0 * mu0);
 
-        std::cout << std::setprecision(10) << "    Speed of light: " << computed_c << std::endl;
+        std::cout << std::setprecision(digits) << "    Speed of light: " << computed_c << std::endl;
     }
 
     // Compute vacuum permittivity:
     {
         fp64 const computed_eps0 = 1.0 / (mu0 * (c * c));
 
-        std::cout << std::setprecision(10) << "    Vacuum permittivity: " << computed_eps0 << std::endl;
+        std::cout << std::setprecision(digits) << "    Vacuum permittivity: " << computed_eps0 << std::endl;
     }
 
     // Compute vacuum permeability:
     {
         fp64 const computed_mu0 = 1.0 / (eps0 * (c * c));
 
-        std::cout << std::setprecision(10) << "    Vacuum permeability: " << computed_mu0 << std::endl;
+        std::cout << std::setprecision(digits) << "    Vacuum permeability: " << computed_mu0 << std::endl;
     }
 
     return EXIT_SUCCESS;
