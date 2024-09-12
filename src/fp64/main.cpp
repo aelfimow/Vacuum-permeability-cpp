81749031caba3f5cca8da0e16ad42dc021de36bc
Alexander Elfimow <my.free.mail@gmx.de>
2024-09-12 20:25:00 +0200

    fp64: added file

---
+++ src/fp64/main.cpp
@@ -0,0 +1,51 @@
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+
+#include "my_types.h"
+
+extern fp64 speed_of_light();
+extern fp64 vacuum_permittivity();
+extern fp64 vacuum_permeability();
+
+int main(int, char *[])
+{
+    fp64 const c = speed_of_light();
+    fp64 const eps0 = vacuum_permittivity();
+    fp64 const mu0 = vacuum_permeability();
+
+    // Print out hard-coded values:
+    {
+        std::cout << "Hard coded values (fp64):" << std::endl;
+
+        std::cout << std::setprecision(10) << "    Speed of light: " << c << std::endl;
+        std::cout << std::setprecision(10) << "    Vacuum permittivity: " << eps0 << std::endl;
+        std::cout << std::setprecision(10) << "    Vacuum permeability: " << mu0 << std::endl;
+    }
+
+    std::cout << "Computed values (fp64):" << std::endl;
+
+    // Compute speed of light:
+    {
+        fp64 const computed_c = 1.0 / ::sqrt(eps0 * mu0);
+
+        std::cout << std::setprecision(10) << "    Speed of light: " << computed_c << std::endl;
+    }
+
+    // Compute vacuum permittivity:
+    {
+        fp64 const computed_eps0 = 1.0 / (mu0 * (c * c));
+
+        std::cout << std::setprecision(10) << "    Vacuum permittivity: " << computed_eps0 << std::endl;
+    }
+
+    // Compute vacuum permeability:
+    {
+        fp64 const computed_mu0 = 1.0 / (eps0 * (c * c));
+
+        std::cout << std::setprecision(10) << "    Vacuum permeability: " << computed_mu0 << std::endl;
+    }
+
+    return EXIT_SUCCESS;
+}
